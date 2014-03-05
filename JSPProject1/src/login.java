843ed0efe093ea1190e699ce4e46343d43a4fff2
koreait09 <koreait09@koreait09-PC>
2014-03-05 15:19:15 +0900

    fddfgret

---
+++ JSPProject1/src/login.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public login() {
+		super();
+		// TODO Auto-generated constructor stub
+	
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			response.setContentType("text/html charset=UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			out.print("id= " + id + "<br>" + "pwd= " + pwd);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
